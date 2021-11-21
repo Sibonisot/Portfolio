72ab276f05f4ea46a4391929fc014d62781a5aae
Siboniso Mabuza <sibonisot@yahoo.com>
2021-11-21 12:23:59 +0200

    Navbar Completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+/*React fontawesone imports*/
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <FontAwesomeIcon icon={faBars}  style={{color: "#fff"}}/>
+        </button>
+      
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Education</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Experience</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacts</a>
+            </li>
+
+            
+       
+          </ul>
+          
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar
